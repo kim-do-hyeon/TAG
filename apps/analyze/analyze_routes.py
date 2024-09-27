7ab9388d1d05fe81c5762696c536c11f746f61e7
pental <pental@kakao.com>
2024-09-27 21:39:06 +0900

    [Refactor] case analyze routes

--- apps/analyze/analyze_routes.py
+++ apps/analyze/analyze_routes.py
@@ -5,6 +5,7 @@ from apps import db
 from apps.case.case_analyze import case_analyze_view
 from apps.case.case_analyze_RAG import search_query
 from apps.analyze.analyze_usb import usb_connection
+from apps.analyze.analyze_filtering import analyze_case_filtering
 import threading
 from flask import current_app
 
@@ -15,6 +16,36 @@ def create_dict_from_file_paths(file_path):
     file_name_without_ext = os.path.splitext(file_name)[0]
     # 딕셔너리 형태로 반환
     return {file_name_without_ext: file_path}
+
+def redirect_analyze_usb(data, progress):
+    case_id = data.get('case_id')
+    if UsbData.query.filter_by(case_id = case_id).first() :
+        progress[case_id] = 100
+        return jsonify({'success': True})
+    db_path = Normalization.query.filter_by(normalization_definition=case_id).first().file
+    user = session.get('username')  # Assuming 'username' is stored in session
+    if not user:
+        flash('사용자 정보를 찾을 수 없습니다. 다시 로그인 해주세요.', 'danger')
+        return redirect('/case/list')
+
+    # Get the current Flask app
+    app = current_app._get_current_object()
+
+    # Create and start a new thread for the search query
+    thread = threading.Thread(target=handle_usb_data, args=(app, db_path, case_id, user, progress))
+    thread.start()
+    return jsonify({'success': True})
+
+def handle_usb_data(app, db_path, case_id, user, progress):
+    global usb_path
+    with app.app_context():
+        progress[case_id] = 0
+        result = usb_connection(db_path, case_id, user, progress)
+        if result :
+            progress[case_id] = 100
+        else :
+            progress[case_id] = 0
+
 
 def redirect_analyze_usb_result(id, user) :
     case_number = Upload_Case.query.filter_by(id = id).first().case_number
--- apps/case/case_analyze_routes.py
+++ apps/case/case_analyze_routes.py
@@ -143,31 +143,4 @@ def redirect_analyze_prompt(data, progress):
 
     return jsonify({'success': True})
 
-def redirect_analyze_usb(data, progress):
-    case_id = data.get('case_id')
-    if UsbData.query.filter_by(case_id = case_id).first() :
-        progress[case_id] = 100
-        return jsonify({'success': True})
-    db_path = Normalization.query.filter_by(normalization_definition=case_id).first().file
-    user = session.get('username')  # Assuming 'username' is stored in session
-    if not user:
-        flash('사용자 정보를 찾을 수 없습니다. 다시 로그인 해주세요.', 'danger')
-        return redirect('/case/list')
 
-    # Get the current Flask app
-    app = current_app._get_current_object()
-
-    # Create and start a new thread for the search query
-    thread = threading.Thread(target=handle_usb_data, args=(app, db_path, case_id, user, progress))
-    thread.start()
-    return jsonify({'success': True})
-
-def handle_usb_data(app, db_path, case_id, user, progress):
-    global usb_path
-    with app.app_context():
-        progress[case_id] = 0
-        result = usb_connection(db_path, case_id, user, progress)
-        if result :
-            progress[case_id] = 100
-        else :
-            progress[case_id] = 0
